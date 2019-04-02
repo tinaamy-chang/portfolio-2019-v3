026dbb4c6fac0d7351c019ed10e2a8adaa55ba62
Scott Wey <scottwey@roaminsight.com>
2019-04-02 11:14:29 -0700

    try to fix mysteriously vanishing hamburger

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,39 +3,20 @@ import styled from 'styled-components';
 import { slide as Menu } from 'react-burger-menu';
 import { Link, withPrefix } from 'gatsby';
 
-const burgerBarClassName = 'burger-bar-class';
-const burgerButtonClassName = 'burger-button-class';
-const StyleWrapper = styled('div')`
-  .${burgerButtonClassName} {
-    @media screen and (min-width: 600px) {
-      display: none;
-    }
-  }
-  .${burgerBarClassName} {
-    background: black;
-  }
-`;
-
 export default props => {
   return (
-    <StyleWrapper>
-      <Menu
-        burgerBarClassName={burgerBarClassName}
-        burgerButtonClassName={burgerButtonClassName}
-        right={props.right}
-      >
-        <a className="menu-item" href="/">
-          Home
-        </a>
+    <Menu right={props.right}>
+      <a className="menu-item" href="/">
+        Home
+      </a>
 
-        <a className="menu-item" href="/designs">
-          Designs&etc
-        </a>
+      <a className="menu-item" href="/designs">
+        Designs&etc
+      </a>
 
-        <a className="menu-item" href={withPrefix('files/TChang_Resume.pdf')}>
-          Resume
-        </a>
-      </Menu>
-    </StyleWrapper>
+      <a className="menu-item" href={withPrefix('files/TChang_Resume.pdf')}>
+        Resume
+      </a>
+    </Menu>
   );
 };
